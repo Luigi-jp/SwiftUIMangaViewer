a4f9b31861fc0ee16642efe79e06733ee42b7280
luigi sato <base.basketball0607@gmail.com>
2025-09-21 20:55:29 +0900

    add: TopView

--- SwiftUIMangaViewer/SwiftUIMangaViewerApp.swift
+++ SwiftUIMangaViewer/SwiftUIMangaViewerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIMangaViewerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TopView()
         }
     }
 }
---
+++ SwiftUIMangaViewer/TopView.swift
@@ -0,0 +1,22 @@
+//
+//  TopView.swift
+//  SwiftUIMangaViewer
+//
+//  Created by 佐藤瑠偉史 on 2025/09/21.
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        Button {
+            // action
+        } label: {
+            Text("Viewerを起動")
+        }
+    }
+}
+
+#Preview {
+    TopView()
+}
