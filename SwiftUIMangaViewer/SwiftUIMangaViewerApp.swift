d3a4f40160442328f698ba617ef0906fc47863c4
luigi sato <base.basketball0607@gmail.com>
2025-09-21 19:47:24 +0900

    initial commit

---
+++ SwiftUIMangaViewer/SwiftUIMangaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMangaViewerApp.swift
+//  SwiftUIMangaViewer
+//
+//  Created by 佐藤瑠偉史 on 2025/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMangaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
